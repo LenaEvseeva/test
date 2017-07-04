ea3fc81a100d1487995f109de763088a195f2cbe
Evseeva Lena <evseeva.elena.1989@gmail.com>
2017-07-04 10:27:47 +0300

    Add todo for exactfarming

--- todo/app/components/todo-ul.js
+++ todo/app/components/todo-ul.js
@@ -1,14 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    toggle(e) {
-      if (e.target.checked) {
-        e.target.nextElementSibling.classList.add("done");
-      }
-      else {
-        e.target.nextElementSibling.classList.remove("done");
-      }
+    actions: {
+        toggle(e) {
+            if (e.target.checked) {
+                e.target.nextElementSibling.classList.add("done");
+            } else {
+                e.target.nextElementSibling.classList.remove("done");
+            }
+        }
     }
-  }
 });
--- todo/app/controllers/todo-list.js
+++ todo/app/controllers/todo-list.js
@@ -1,35 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    addTodo(e) {
-      e.preventDefault();
+    actions: {
+        addTodo(e) {
+            e.preventDefault();
 
-      if (!this.newTodoTitle) {
-        return;
-      }
+            if (!this.newTodoTitle) {
+                return;
+            }
 
-      this.model.todoList.pushObject({
-        title: this.newTodoTitle,
-        isComplete: false
-      });
-    },
-    handleInput(e) {
-      this.newTodoTitle = e.target.value;
-    },
-    deleteTodo(e) {
+            this.model.todoList.pushObject({
+                title: this.newTodoTitle,
+                isComplete: false
+            });
+        },
+        handleInput(e) {
+            this.newTodoTitle = e.target.value;
+        },
+        deleteTodo(e) {
 
-    var textInInput = e.target.previousElementSibling.textContent;
-    for (var i = 0; i < this.model.todoList.length; i++) {
+            var textInInput = e.target.previousElementSibling.textContent;
 
-      if (textInInput == this.model.todoList.title) {
-        this.model.todoList.removeAt(i,1);
+            for (var i = 0; i < this.model.todoList.length; i++) {
+
+                if (textInInput === this.model.todoList[i].title) {
+                    this.model.todoList.removeAt(i, 1);
+                }
+            }
+        }
     }
-  }
-      console.log(this.model.todoList[new Object.title]);
-    },
-    handleInput(e) {
-      this.newTodoTitle = e.target.value;
-    }
-  }
 });
